7fbde84c23cf806843243082e5409678670550c8
K.Kiyokawa <koichi20110068@gmail.com>
2022-01-18 14:25:23 +0900

    chore(smui6): init

--- smui-ts/src/routes/__layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import '@smui/button/bare.css';
-	import '@smui/checkbox/bare.css';
-	import '@smui/data-table/bare.css';
-</script>
-
-<slot />
--- smui6/svelte.config.js
+++ smui6/svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +8,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter()
+		target: '#svelte'
 	}
 };
 
