fea9d767f1083ee79caacf7d3233c93d97dd82c3
K.Kiyokawa <koichi20110068@gmail.com>
2022-01-18 14:41:57 +0900

    chore(smui6): setup

--- smui6/src/routes/index.svelte
+++ smui6/src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button variant="raised">ok</Button>
