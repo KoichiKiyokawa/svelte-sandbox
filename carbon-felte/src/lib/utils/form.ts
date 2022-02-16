cebc6f5c25d28367ded65ca61cf90380a18f5044
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-02-16 19:35:48 +0900

    chore: update felte and svelte.config

--- carbon-felte/src/lib/utils/form.ts
+++ carbon-felte/src/lib/utils/form.ts
@@ -6,7 +6,7 @@ export const createForm = <Schema>(arg: Parameters<typeof baseCreateForm>[0]) =>
 	return {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		...baseCreateForm<Schema, ValidatorConfig>(arg as any),
-		extend: validator,
+		extend: validator({ schema: arg?.schema }),
 		nameAttribute: (key: keyof Schema) => ({ name: key })
 	};
 };
