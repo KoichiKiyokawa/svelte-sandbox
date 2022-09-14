7808531cea081012949e55fa75046e01e5e5d359
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-09-14 20:14:09 +0900

    chore: remove `svelte-kit-cookie-session`

--- svelte-kit-prisma/src/app.d.ts
+++ svelte-kit-prisma/src/app.d.ts
@@ -1,18 +1,1 @@
 /// <reference types="@sveltejs/kit" />
-
-interface SessionData {
-	// Your session data
-	userId?: string;
-}
-
-// See https://kit.svelte.dev/docs#typescript
-// for information about these interfaces
-declare namespace App {
-	interface Locals {
-		session: import('svelte-kit-cookie-session').Session<SessionData>;
-		cookies: Record<string, string>; // all parsed cookies are automatically set from handleSession to avoid overhead
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-empty-interface
-	interface Session extends SessionData {}
-}
