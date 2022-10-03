162e5c73e208ebd87c033c81ce3c089e11569a57
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-03 23:49:09 +0900

    chore(svelte-blues-stack): setup vitest

---
+++ svelte-blues-stack/src/componentes/Counter/Counter.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest';
+import { render, fireEvent, screen } from '@testing-library/svelte';
+import Counter from './index.svelte';
+
+describe('Counter', () => {
+	const getCurrentCount = () => screen.getByTestId('counter-state').textContent;
+	render(Counter, { props: {} });
+
+	it('can increment', async () => {
+		const incrementButton = screen.getByRole('button', { name: '+1' });
+		await fireEvent.click(incrementButton);
+
+		const currentCountElement = screen.getByTestId('counter-state');
+		expect(currentCountElement.textContent).toBe('1');
+
+		await fireEvent.click(incrementButton);
+		expect(currentCountElement.textContent).toBe('2');
+	});
+
+	const decrementButton = screen.getByRole('button', { name: '-1' });
+	it('can decrement', async () => {
+		await fireEvent.click(decrementButton);
+
+		expect(getCurrentCount()).toBe('1');
+
+		await fireEvent.click(decrementButton);
+		expect(getCurrentCount()).toBe('0');
+	});
+
+	it('should not became smaller than 0', async () => {
+		await fireEvent.click(decrementButton);
+		expect(getCurrentCount()).toBe('0');
+
+		await fireEvent.click(decrementButton);
+		expect(getCurrentCount()).toBe('0');
+	});
+});
---
+++ svelte-blues-stack/src/componentes/Counter/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let name: string;
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+
+	function decrement() {
+		if (count >= 1) count -= 1;
+	}
+</script>
+
+<div>
+	<h3>{name}</h3>
+	<button on:click={decrement}>-1</button>
+	<span data-testid="counter-state">{count}</span>
+	<button on:click={increment}>+1</button>
+</div>
