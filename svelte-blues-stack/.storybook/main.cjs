fbf9439f8f42fc66327e43d7b9e4ea9b97324148
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-04 01:53:37 +0900

    chore: add storybook's a11y plugin

--- svelte-blues-stack/.storybook/main.cjs
+++ svelte-blues-stack/.storybook/main.cjs
@@ -1,12 +1,8 @@
 const preprocess = require('svelte-preprocess');
 
 module.exports = {
-	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
-	],
+	stories: ['../src'],
+	addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: '@storybook/svelte',
 	core: {
 		builder: '@storybook/builder-vite'
