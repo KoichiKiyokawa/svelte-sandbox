0b075dab035248b0012d41852f7db802cb1bc366
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-09 16:22:23 +0900

    chore: prisma log setting

--- svelte-kit-prisma/src/lib/utils/db.server.ts
+++ svelte-kit-prisma/src/lib/utils/db.server.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaClient();
 } else {
 	if (!globalThis.prisma) {
-		globalThis.prisma = new PrismaClient();
+		globalThis.prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
 	}
 	prisma = globalThis.prisma;
 }
