840eaa7398396de483db19cc5bf1e6ef00d51fa1
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-25 00:23:32 +0900

    fix: use max-width

--- image-tools/src/routes/+page.svelte
+++ image-tools/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Picture from '../assets/4k.jpg?w=320;768;1024;2048&format=webp;avif&picture';
+	import Picture from '../assets/4k.jpg?w=320;768;1024;2048&format=webp;avif;jpg&picture';
 	console.log(JSON.stringify(Picture, null, 2));
 </script>
 
 <picture>
 	{#each Object.entries(Picture.sources) as [format, images]}
 		{#each images as image}
-			<source srcset={image.src} media="(min-width: {image.w}px)" type="image/{format}" />
+			<source srcset={image.src} media="(max-width: {image.w}px)" type="image/{format}" />
 		{/each}
 	{/each}
 	<img src={Picture.fallback.src} alt="" />
