f2dd9cb4991b6d192cd637173df0666a25ff852f
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-12-17 19:39:06 +0900

    feat: use `fail` method

--- svelte-kit-prisma/src/routes/login/+page.server.ts
+++ svelte-kit-prisma/src/routes/login/+page.server.ts
@@ -1,7 +1,7 @@
 import { CookieKeys } from '$lib/constants/cookie';
 import { db } from '$lib/utils/db.server';
 import { jsonifyRequest } from '$lib/utils/request.server';
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import bcrypt from 'bcryptjs';
 import dayjs from 'dayjs';
 import type { Actions } from './$types';
@@ -10,7 +10,7 @@ export const actions: Actions = {
 	async default({ request, cookies }) {
 		const { email, password } = await jsonifyRequest<{ email: string; password: string }>(request);
 		const targetUser = await db.user.findUnique({ where: { email } });
-		const noUserResponse = { error: 'No user found' };
+		const noUserResponse = fail(403, { error: 'No user found' });
 
 		if (targetUser === null) return noUserResponse;
 		if (!bcrypt.compareSync(password, targetUser.passwordHash)) return noUserResponse;
